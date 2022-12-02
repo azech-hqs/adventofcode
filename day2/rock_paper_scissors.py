2a57bc28363fcf7ed58b9ead78b16964dc899e60
Alexander Zech <alexzech@gmx.de>
2022-12-02 12:10:45 -0800

    chore: working version for part2

--- day2/rock_paper_scissors.py
+++ day2/rock_paper_scissors.py
@@ -3,9 +3,9 @@ import sys
 
 
 POINTS = {
-    "X": 1,
-    "Y": 2,
-    "Z": 3,
+    "X": 1, # A rock
+    "Y": 2, # B paper
+    "Z": 3, # C scissors
 }
 
 
@@ -33,20 +33,49 @@ ROUND_POINTS = {
     "Z": 6,
 }
 
-# make a map of [A][Y] -> A
+SHAPE_VALUE = {
+    "A": 1,
+    "B": 2,
+    "C": 3,
+}
 
-def calculate_score(shapes: Tuple[str, str]) -> int:
-    p1, p2 = shapes
+RESPONSE = {
+    "A": {
+        "X": "C",
+        "Y": "A",
+        "Z": "B",
+    },
+    "B": {
+        "X": "A",
+        "Y": "B",
+        "Z": "C",
+    },
+    "C": {
+        "X": "B",
+        "Y": "C",
+        "Z": "A",
+    },
+}
+
+def calculate_score(p1: str, p2: str) -> int:
     return OUTCOME[p1][p2] + POINTS[p2]
 
+def calculate_score_2(p1: str, outcome: str) -> int:
+    response = RESPONSE[p1][outcome]
+    print(f"player1 played {p1}, your response is {response}")
+    return ROUND_POINTS[outcome] + SHAPE_VALUE[response]
 
 if __name__ == "__main__":
     with open(sys.argv[1], "r") as f:
         content = f.read()
 
     games = [line.split(" ") for line in content.rstrip("\n").split("\n")]
-    game_points_1 = [calculate_score(game) for game in games]
-    game_points_2 = []
+    game_points_1 = [calculate_score(*game) for game in games]
+    game_points_2 = [calculate_score_2(*game) for game in games]
 
     print(f"There have been {len(game_points_1)} games.")
     print(f"You gained {sum(game_points_1)} points.")
+    print(20*"-")
+    print(f"Ultra top secret strategy:")
+    print(f"Your total score is {sum(game_points_2)} points.")
+
