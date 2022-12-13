334fe790427851795b7abf8affa9803d7d1bc62c
Alexander Zech <alexzech@gmx.de>
2022-12-12 22:18:43 -0800

    chore: solutions for day9 part 2

--- day9/rope_physics.py
+++ day9/rope_physics.py
@@ -18,9 +18,11 @@ MOVE = {
 
 
 class Rope:
-    def __init__(self) -> None:
+    def __init__(self, n_knots: int = 2) -> None:
         self.head = np.array([0,0])
         self.tail = np.array([0,0])
+        self.n_knots = n_knots
+        self.knots = np.zeros((n_knots, 2), dtype=int)
         self.visited = []
 
     def move(self, direction: str, steps: int) -> None:
@@ -30,6 +32,28 @@ class Rope:
             self.move_tail()
             self.add_to_visited(self.tail)
             self.print()
+
+    def move_alt(self, direction: str, steps: int) -> None:
+        print(f"== {direction} {steps} ==")
+        for i in range(0, steps):
+            self.knots[0] += MOVE[direction]
+            for k in range(self.n_knots):
+                self.move_knot(k)
+            self.add_to_visited(self.knots[-1])
+            # self.print_knots()
+
+    def move_knot(self, index: int) -> None:
+        if index == 0:
+            return
+        diff = self.knots[index-1] - self.knots[index]
+        if np.all(np.abs(diff) <= 1):
+            return
+        comps = diff * np.identity(2)
+        for i in range(2):
+            if any(comps[i]):
+                d = comps[i] / np.linalg.norm(comps[i])
+                self.knots[index] += d.astype(int)
+        
 
     def move_tail(self) -> None:
         diff = self.head - self.tail
@@ -50,10 +74,21 @@ class Rope:
         print(f"T: ({self.tail[0]},{self.tail[1]})")
         print("-"*10)
 
+    def print_knots(self):
+        print(self.knots)
+        print("-"*10)
+
 if __name__ == "__main__":
     movements = get_content(sys.argv[1])
-    rope = Rope()
+    # part 1
+    rope = Rope(n_knots=2)
     for movement in movements:
         rope.move(movement[0], int(movement[1]))
     print(f"Tail visited {len(rope.visited)} distinct coordinates")
+
+    # part 2
+    long_rope = Rope(n_knots=10)
+    for movement in movements:
+        long_rope.move_alt(movement[0], int(movement[1]))
+    print(f"Tail visited {len(long_rope.visited)} distinct coordinates")
 
