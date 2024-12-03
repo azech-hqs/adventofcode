d33d634db8248b0a47771dfe4c16207542016397
Alexander Zech <10773967+azech-hqs@users.noreply.github.com>
2024-12-03 21:23:47 +0100

    refactor(2024.1): move total distance to function

--- 2024/day1/src/main.rs
+++ 2024/day1/src/main.rs
@@ -2,6 +2,15 @@ use anyhow::{Ok, Result};
 use std::env;
 use std::{cmp::Reverse, fs::read_to_string};
 
+fn calculate_total_distance(left: &[i32], right: &[i32]) -> i32 {
+    return left
+        .into_iter()
+        .zip(right.into_iter())
+        .map(|(l, r)| (l - r).abs())
+        .into_iter()
+        .sum();
+}
+
 fn main() -> Result<()> {
     let filename: String = env::args().nth(1).unwrap_or(String::from("input.txt"));
 
@@ -24,13 +33,7 @@ fn main() -> Result<()> {
         .collect::<Vec<_>>();
     right.sort_by_key(|x| Reverse(*x));
 
-    let distance_sum: i32 = left
-        .into_iter()
-        .zip(right.into_iter())
-        .map(|(l, r)| (l - r).abs())
-        .into_iter()
-        .sum();
-
+    let distance_sum = calculate_total_distance(&left, &right);
     println!("The total distance is {:?}", distance_sum);
 
     return Ok(());
