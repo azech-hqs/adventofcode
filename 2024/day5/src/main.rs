4b11a75faf99da14d0adb557672db1117448bc52
Alexander Zech <10773967+azech-hqs@users.noreply.github.com>
2024-12-22 11:26:12 +0100

    feat(2024.5): second half of puzzle

--- 2024/day5/src/main.rs
+++ 2024/day5/src/main.rs
@@ -83,6 +83,54 @@ impl Rules {
     fn is_ordered(&self, a: &i32, b: &i32) -> bool {
         return self.data.get(a).map(|v| v.contains(b)).unwrap_or(false);
     }
+
+    fn quick_sort(&self, v: &mut Vec<i32>) -> Vec<i32> {
+        self._qs(v, 0, v.len() - 1);
+        println!("sorted array: {:?}", &v);
+        return v.to_vec();
+    }
+
+    fn _qs(&self, v: &mut Vec<i32>, left_index: usize, right_index: usize) -> Option<()> {
+        if right_index as i32 - left_index as i32 <= 0 {
+            return Some(());
+        }
+        let pivot_index = self._partition_array(v, left_index, right_index)?;
+        self._qs(v, left_index, pivot_index.saturating_sub(1));
+        self._qs(v, pivot_index + 1, right_index);
+        return Some(());
+    }
+
+    fn _partition_array(
+        &self,
+        v: &mut Vec<i32>,
+        left_index: usize,
+        right_index: usize,
+    ) -> Option<usize> {
+        let pivot_index = right_index;
+        let pivot = v.get(pivot_index)?.clone();
+        let mut right_ptr = right_index.saturating_sub(1);
+        let mut left_ptr = left_index;
+
+        loop {
+            while self.is_ordered(&v[left_ptr], &pivot) {
+                left_ptr += 1;
+            }
+
+            while self.is_ordered(&pivot, &v[right_ptr]) && right_ptr != 0 {
+                right_ptr = right_ptr.saturating_sub(1);
+            }
+
+            if left_ptr >= right_ptr {
+                break;
+            } else {
+                v.swap(left_ptr, right_ptr);
+                left_ptr += 1;
+            }
+        }
+
+        v.swap(left_ptr, pivot_index);
+        return Some(left_ptr);
+    }
 }
 
 fn main() -> Result<(), ReadDataError> {
@@ -96,17 +144,27 @@ fn main() -> Result<(), ReadDataError> {
     let (rules, arrays) = read_data()?;
 
     let mut middle: i32 = 0;
+    let mut middle_unsorted: i32 = 0;
     for array in &arrays[..] {
         let is_ordered = array.windows(2).all(|w| rules.is_ordered(&w[0], &w[1]));
         if is_ordered {
             let i = array.len() / 2;
             middle += array[i];
+        } else {
+            let mut sorted = array.clone();
+            rules.quick_sort(&mut sorted);
+            let i = sorted.len() / 2;
+            middle_unsorted += sorted[i];
         }
     }
 
     println!("there are {:?} rules", rules.data.len());
     println!("there are {:?} arrays", arrays.len());
     println!("the sum of middle values is {:?}", &middle);
+    println!(
+        "the sum of middle values (unsorted) is {:?}",
+        &middle_unsorted
+    );
 
     return Ok(());
 }
